95ebf4d16511318d1a2e51c1e2c59d99f93dd7c3
Jia Kai Chang <changjiakai@gmail.com>
2023-05-31 00:06:29 +0800

    first commit

---
+++ 3-10.py
@@ -0,0 +1,18 @@
+def is_anagram(str1, str2):
+
+    if len(str1) != len(str2):
+        return False
+
+    str1_dict = {}
+    str2_dict = {}
+
+    for char in str1:
+        str1_dict[char] = str1_dict.get(char, 0) + 1
+
+    for char in str2:
+        str2_dict[char] = str2_dict.get(char, 0) + 1
+
+    return str1_dict == str2_dict
+
+
+print(is_anagram('cbaa', 'aabc'))
